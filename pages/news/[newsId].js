df532544c38887bf964ef5f88e5309886d9b7989
maryam <50619466+marykhodaparast@users.noreply.github.com>
2022-02-24 12:06:19 +0330

    use fragment

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,12 @@
 //ourdomain.com/news/something-important
 
+import { useRouter } from 'next/router';
+
 function DetailPage(){
+    const router = useRouter();
+    const newsId = router.query.newsId;
+    // send a request to the backend api 
+    // to fetch the newsitem with newsId
     return <h1>The Detail page</h1>
- }
+}
  export default DetailPage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,12 @@
 //ourdomain.com/news
-
+import { Fragment } from 'react';
 function NewsPage(){
-    return <h1>The News page</h1>
+    return <Fragment>
+             <h1>The News page</h1>
+             <ul>
+                 <li><a href="/news/nextjs-is-a-great-framework">NextJs is a great framework</a></li>
+                 <li>Something else</li>
+             </ul>
+           </Fragment>
  }
  export default NewsPage;
