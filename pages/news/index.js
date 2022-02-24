26920386885a6c249cdc8068ff5a3ef81ce00def
maryam <50619466+marykhodaparast@users.noreply.github.com>
2022-02-24 12:10:56 +0330

    having in single application

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,10 +1,11 @@
 //ourdomain.com/news
 import { Fragment } from 'react';
+import Link from 'next/link';
 function NewsPage(){
     return <Fragment>
              <h1>The News page</h1>
              <ul>
-                 <li><a href="/news/nextjs-is-a-great-framework">NextJs is a great framework</a></li>
+                 <li><Link href="/news/nextjs-is-a-great-framework">NextJs is a great framework</Link></li>
                  <li>Something else</li>
              </ul>
            </Fragment>
